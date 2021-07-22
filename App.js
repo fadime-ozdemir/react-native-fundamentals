63fabecee34bc365adc1941d014b91d62236683a
fadime94 <fadime.ozdemir11@gmail.com>
2021-07-22 16:09:52 +0300

    add starting code

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => (
+  <Text style={{ margin: 64 }}>
+    Welcome to React Native!
+  </Text>
+);
+
+export default App;
